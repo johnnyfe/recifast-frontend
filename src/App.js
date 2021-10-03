61e96d43003f249d99fef9255e4c40deebaeca95
johnny <johnny1194@hotmail.com>
2021-10-03 19:53:53 -0400

    removing unnecesary code in app js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
-import { useEffect, useState } from "react";
 import './App.css';
 import RecipeContainer from "./components/RecipeContainer";
-import Login from "./components/Login";
 
 function App() {
-
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    // auto-login
-    fetch("/me").then((r) => {
-      if (r.ok) {
-        r.json().then((user) => setUser(user));
-      }
-    });
-  }, []);
-
-  if (!user) return <Login onLogin={setUser} />;
 
   return (
     <Router>
