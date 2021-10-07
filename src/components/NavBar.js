43e1a0ef9d6e2d8be8b742dee3e9f56fbdf69938
johnny <johnny1194@hotmail.com>
2021-10-07 14:25:50 -0400

    adding navbar css toapp and font style to html

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from "../images/Recifast-Logo.png"
+import "../style/Navbar.css"
 
 function NavBar() {
     
     return (
         <div className="navbar">
-            <Link to="/"><img src={logo} alt="recifast-logo"></img></Link>
-            <Link to="/recipes">Recipes</Link>
-            <Link to="/ingredients">Ingredients</Link>
+                <img className="logo-image" src={logo} alt="recifast-logo"></img>
+            <ul>
+                <li><a><Link to="/">Homepage</Link></a></li>
+                <li><a><Link to="/recipes">Recipes</Link></a></li>
+                <li><a><Link to="/ingredients">Ingredients</Link></a></li>
+            </ul>
         </div>
     );
 }
