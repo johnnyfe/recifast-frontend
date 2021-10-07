ead7f842c750fb7f63e7c3c12a425576190f49d6
johnny <johnny1194@hotmail.com>
2021-10-07 16:23:04 -0400

    adding homepage css to app and another font to html

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import phone from "../images/phone-mockup.png"
+import "../style/HomePage.css"
 
 function HomePage() {
     return (
         <div className="homepage">
+            <div className="text-box-homepage">
+               <div className="details-homepage">
+                    <h1>ReciFast</h1>
+                    <h3>Welcome to the social media of recipes</h3>
+                    <h3>Where you can create your own recipes</h3>
+                    <h3>Share and Enjoy</h3>
+                    <h3>Help others to make delicious food at home</h3>
+                </div>
+                <div className="about-homepage">
+                    <h2>About</h2>
+                    <h3>ReciFast is a great website to learn how to make your favorite recipes without any experience</h3>  
+                </div> 
+            </div>
+            
             <img src ={phone} alt="phone-mockup"></img>
-            <h1>ReciFast</h1>
-            <h4>Welcome to the social media of recipes</h4>
-            <h4>Where you can create your own recipes</h4>
-            <h4>Share and Enjoy</h4>
-            <h4>Help others to make delicious food at home</h4>
         </div>
     );
 }
